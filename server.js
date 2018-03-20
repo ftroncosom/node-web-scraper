a9bcb2aac49253d7543b56507ef44bba35a2d6f4
Francisco Troncoso <francisco.troncoso@scotiabank.cl>
2018-03-20 10:32:32 -0300

    correccion de la iteracion y generacion de un json array

--- server.js
+++ server.js
@@ -3,19 +3,32 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var list = new Array();
 
 app.get('/scrape', function(req, res){
   // Let's scrape Anchorman 2
-  url = 'http://www.imdb.com/title/tt1229340/';
+  url = 'https://si3.bcentral.cl/indicadoressiete/secure/indicadoresdiarios.aspx';
 
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
 
       var title, release, rating;
-      var json = { title : "", release : "", rating : ""};
+      
 
-      $('.title_wrapper').filter(function(){
+      $("tr.filas_indicadores").filter(function(){
+        var data = $(this);
+        var json = { name : "", value : ""};  
+
+        name = data.children().first().text();
+        value = data.children().first().next().text();
+        json.name = name;
+        json.value = value;
+        list.push(json);
+
+        console.log('json => ' + JSON.stringify(json, null, 4));  
+      });
+      /*$('.title_wrapper').filter(function(){
         var data = $(this);
         title = data.children().first().text().trim();
         release = data.children().last().children().last().text().trim();
@@ -29,10 +42,14 @@ app.get('/scrape', function(req, res){
         rating = data.text().trim();
 
         json.rating = rating;
-      })
-    }
+      })*/
 
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+      console.log('json => ' + JSON.stringify(list, null, 4));  
+    }
+    
+    
+
+    fs.writeFile('output.json', JSON.stringify(list, null, 4), function(err){
       console.log('File successfully written! - Check your project directory for the output.json file');
     })
 
